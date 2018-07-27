6d530aeac6ebbf93f5837c0c7080ab9f1a22ed06
yuki-s-y <yuki_yamamoto@adoc.co.jp>
2018-07-27 11:39:37 +0900

    Update teardown orchestratioin script.

--- __main__.py
+++ __main__.py
@@ -1,23 +1,23 @@
 from cloudshell.workflow.orchestration.sandbox import Sandbox
-from cloudshell.workflow.orchestration.setup.default_setup_orchestrator import DefaultSetupWorkflow
+from cloudshell.workflow.orchestration.teardown.default_teardown_orchestrator import DefaultTeardownWorkflow
 import Proc_L1ConnectionController as L1ConnectionController
 from Work_Config import WorkConfig
 
 
 def main():
-# Default Setup Process
+    # Default Setup Process
     sandbox = Sandbox()
-    DefaultSetupWorkflow().register(sandbox)
-    sandbox.execute_setup()
+    DefaultTeardownWorkflow().register(sandbox)
+    sandbox.execute_teardown()
 
-# Additional Setup Process
-# Connect All L1 Routes
-    L1ConnectionController.ChangeStateOfAllL1Routes("Connect")
+    # Additional Setup Process
+    # Connect All L1 Routes
+    L1ConnectionController.ChangeStateOfAllL1Routes("Disconnect")
 
-# Set Config
-# insert code here
+    # Set Config
+    # insert code here
     work_config = WorkConfig()
-    work_config.input_config_all("Config Path")
+    work_config.input_config_all("Init Config Path")
 
 
 # Entry Point
